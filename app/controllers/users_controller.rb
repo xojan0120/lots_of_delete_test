2b333b095863e77f82d868d37879c020c82684f0
xojan0120 <xojan0120@gmail.com>
2019-02-22 18:04:05 +0900

    [Update] dependentオプションでdestroy->delete_allにする方法に変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,11 +54,8 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    #@user.destroy
+    @user.destroy
     
-    @user.microposts.delete_all
-    @user.delete
-
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :microposts, dependent: :destroy
+  #has_many :microposts, dependent: :destroy
+  has_many :microposts, dependent: :delete_all
 end
